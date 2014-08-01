934f2c45bd5815bee426faa8084dbd4a77519482
danwardvs <danwardvs@adsgames.net>
2014-08-01 16:05:55 -0700

    Added a font to the title

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 BITMAP* title_screen;
 BITMAP* buffer;
 
+FONT* font_48;
+
 void abort_on_error(const char *message){
 	 if (screen != NULL){
 	    set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
@@ -20,6 +22,23 @@ void setup(){
     abort_on_error( "Cannot find images/title_screen.png.\n Please check your files and try again.");
   }
 
+  FONT *f1, *f2, *f3, *f4, *f5;
+
+  if(!(f1 = load_font("fonts/font_48.pcx", NULL, NULL))){
+    abort_on_error( "Cannot find fonts/font_48.pcx \n Please check your files and try again");
+  }
+  f2 = extract_font_range(f1, ' ', 'A'-1);
+  f3 = extract_font_range(f1, 'A', 'Z');
+  f4 = extract_font_range(f1, 'Z'+1, 'z');
+
+  //Merge temporary fonts to create "pixelart"
+  font_48 = merge_fonts(f4, f5 = merge_fonts(f2, f3));
+
+  destroy_font(f1);
+  destroy_font(f2);
+  destroy_font(f3);
+  destroy_font(f4);
+  destroy_font(f5);
 
 
 }
@@ -48,6 +67,9 @@ int main(){
 
 
         stretch_sprite(buffer,title_screen,0,0,SCREEN_W,SCREEN_H);
+        textprintf_ex(buffer,font_48,20,20,makecol(255,255,255),makecol(0,0,0),"3 5 7");
+
+
 
         draw_sprite(screen,buffer,0,0);
 
